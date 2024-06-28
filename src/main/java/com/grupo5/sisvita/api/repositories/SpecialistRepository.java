e284500e64f558f8edc6b611ae35fa85f259d1d0
BrandonVera10 <brandon.vera@unmsm.edu.oe>
2024-06-28 17:16:19 -0500

    fix repositories

--- src/main/java/com/grupo5/sisvita/api/repositories/SpecialistRepository.java
+++ src/main/java/com/grupo5/sisvita/api/repositories/SpecialistRepository.java
@@ -2,6 +2,11 @@ package com.grupo5.sisvita.api.repositories;
 
 import com.grupo5.sisvita.api.entities.Specialist;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface SpecialistRepository extends JpaRepository<Specialist, Long> {
+
+    @Query("SELECT s.id FROM Specialist s WHERE s.user.id = :user_id")
+    Long findIdByIdUser(@Param("user_id") Long userId);
 }
--- src/main/java/com/grupo5/sisvita/api/repositories/TemplateTestRepository.java
+++ src/main/java/com/grupo5/sisvita/api/repositories/TemplateTestRepository.java
@@ -2,6 +2,15 @@ package com.grupo5.sisvita.api.repositories;
 
 import com.grupo5.sisvita.api.entities.TemplateTest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TemplateTestRepository extends JpaRepository<TemplateTest, Long> {
+
+    @Query("SELECT t.name FROM TemplateTest t")
+    List<String> findAllName();
+
+    @Query("SELECT c.interpretation FROM Classification c JOIN TemplateTest t ON c.templateTest.id = t.id WHERE t.name = :name")
+    List<String> findClassificationNameByName(String name);
 }
