e02e53040fa6654a17abfda8ace6c9b77efafcac
Fabo2303 <fabian666777@gmail.com>
2024-07-05 13:53:28 -0500

    add Authentication Request and Response

---
+++ src/main/java/com/example/sisvita/auth/dto/AuthenticationRequest.java
@@ -0,0 +1,13 @@
+package com.example.sisvita.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/example/sisvita/auth/dto/AuthenticationResponse.java
@@ -0,0 +1,12 @@
+package com.example.sisvita.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String jwt;
+}
